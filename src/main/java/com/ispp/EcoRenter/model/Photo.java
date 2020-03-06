1d856cf9e1f2632574d7fdcdd21f28109bcd6142
alvcalgon <alvcalgon@alum.us.es>
2020-03-06 20:39:46 +0100

    [feat] Actualizado definición de la entidad Photo

--- src/main/java/com/ispp/EcoRenter/model/Photo.java
+++ src/main/java/com/ispp/EcoRenter/model/Photo.java
@@ -6,6 +6,7 @@ import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Access(AccessType.PROPERTY)
@@ -13,47 +14,47 @@ public class Photo extends DomainEntity {
 
 	private static final long serialVersionUID = 1L;
 
+	// Attributes ------------------------------------
+	
 	@NotBlank
-	private String name;
-	private String suffix;
-	private String content;
-	private Blob structure;
+	private String title;
+	
+	@NotBlank
+	private String format;
+	
+	@NotNull
+	private Blob data;
+	
+	// Constructors ----------------------------------
 	
 	public Photo() {
 		super();
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getSuffix() {
-		return suffix;
-	}
-
-	public void setSuffix(String suffix) {
-		this.suffix = suffix;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public Blob getStructure() {
-		return structure;
-	}
-
-	public void setStructure(Blob structure) {
-		this.structure = structure;
-	}
+	// Getters and setters --------------------------
 	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getFormat() {
+		return format;
+	}
+
+	public void setFormat(String format) {
+		this.format = format;
+	}
+
+	public Blob getData() {
+		return data;
+	}
+
+	public void setData(Blob data) {
+		this.data = data;
+	}
 	
 }
