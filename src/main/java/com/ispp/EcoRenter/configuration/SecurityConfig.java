29163245075b3e163beb9a741625047e40326236
Carlos <carlos@gmail.com>
2020-03-09 02:27:29 +0100

    [feat] login con disabled users

--- src/main/java/com/ispp/EcoRenter/configuration/SecurityConfig.java
+++ src/main/java/com/ispp/EcoRenter/configuration/SecurityConfig.java
@@ -1,60 +1,74 @@
 package com.ispp.EcoRenter.configuration;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
- 
+
+
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+
+	@Autowired
+	DataSource dataSource;
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/").permitAll().
-				antMatchers("/resources/**").permitAll()
+				.antMatchers("/").permitAll()
+				.antMatchers("/resources/**").permitAll()
+				.antMatchers("/admin").hasAuthority("ADMIN")
+				.antMatchers("/renter").hasAuthority("RENTER")
+				.antMatchers("/owner").hasAuthority("OWNER")
 				.anyRequest().authenticated()
 				.and()
 			.formLogin()
-//				.loginPage("/login")
+//			.loginPage("/login")
 				.and()
 			.logout()
-				.logoutSuccessUrl("/");
-	}
-
-	/*
-	 * Este método simula que hay un usuario en la base de datos con:
-	 * usuario: user
-	 * contraseña: password
-	 * rol: USER
-	 * 
-	 * Debe eliminarse una vez que esté implementada la lógica de 
-	 * usuarios para que utilice la base de datos*/
-	@Bean
-	@Override
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
-				.build();
-
-		return new InMemoryUserDetailsManager(user);
+				;
 	}
 	
+	
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		
+		auth.jdbcAuthentication()
+		.dataSource(dataSource)	
+		.usersByUsernameQuery("select username,password,is_banned "
+				+ "from user_account "
+				+ "where username = ?")
+		.authoritiesByUsernameQuery("select u.username, a.authority "
+				+ "from eco_renter.user_account u "
+				+ "join eco_renter.user_account_authorities a "
+				+ "on u.id = a.user_account_id "
+				+ "where username = ?"); 
+			
+		
+		
+	}
+
+
+
 	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+	public PasswordEncoder passwordEncoder() {	    
+		PasswordEncoder encoder =  NoOpPasswordEncoder.getInstance();
+	    return encoder;
 	}
 	
 }
