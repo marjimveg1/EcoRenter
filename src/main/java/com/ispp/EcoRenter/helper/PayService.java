1cc88ecf336f6cf2cc423fdfbc2d5abd7252aa5d
Carlos <carlos@gmail.com>
2020-03-15 02:54:45 +0100

    [feat] Implementado la simulacion de un pago con stripe, sin que rediriga a la pagina de exito

---
+++ src/main/java/com/ispp/EcoRenter/helper/PayService.java
@@ -0,0 +1,38 @@
+package com.ispp.EcoRenter.helper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.stripe.Stripe;
+import com.stripe.exception.AuthenticationException;
+import com.stripe.exception.CardException;
+import com.stripe.exception.InvalidRequestException;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
+
+@Service
+public class PayService {
+
+    @Value("pk_test_76RlPdxYR4nMYOpmKbYA8xE9000VDDeCpk")
+    String secretKey;
+
+    @PostConstruct
+    public void init() {
+        Stripe.apiKey = secretKey;
+    }
+
+    public Charge charge(ChargeRequest chargeRequest)
+            throws AuthenticationException, InvalidRequestException, CardException, StripeException{
+        Map<String, Object> chargeParams = new HashMap<>();
+        chargeParams.put("amount", chargeRequest.getAmount());
+        chargeParams.put("currency", chargeRequest.getCurrency());
+        chargeParams.put("description", chargeRequest.getDescription());
+        chargeParams.put("source", chargeRequest.getStripeToken());
+        return Charge.create(chargeParams);
+    }
+}
